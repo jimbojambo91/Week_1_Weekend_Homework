99f348e84b01040d7139eea166af2b410a4df6fd
James Duncan <james.a.duncan91@gmail.com>
2017-03-10 10:59:51 +0000

    adding code file, passed five tests

--- pet_shop.rb
+++ pet_shop.rb
@@ -3,5 +3,17 @@ def pet_shop_name(pet_shop)
 end
 
 def total_cash(pet_shop)
-  
+  return pet_shop[:admin][:total_cash] 
+end
+
+# def add_or_remove_cash(pet_shop, cash)
+#   return pet_shop[:admin][:total_cash] += cash
+# end
+
+# def add_or_remove_cash(pet_shop, cash)
+#   return pet_shop[:admin][:total_cash] -= cash
+# end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
 end
