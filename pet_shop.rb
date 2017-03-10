4c4326179d9f624c6879c5c61d86ec68be683bd2
James Duncan <james.a.duncan91@gmail.com>
2017-03-10 14:25:53 +0000

    adding functions to final solution

--- pet_shop.rb
+++ pet_shop.rb
@@ -42,13 +42,13 @@ def find_pet_by_name(pet_shop, name)
   return 
 end
 
-# def remove_pet_by_name(pet_shop, name)
-#   for pet in pet_shop[:pets]
-#     if pet[:name] == name
-#       pet_shop[:pets].delete(pet)
-#     end
-#   end
-# end
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      pet_shop[:pets].delete(pet)
+    end
+  end
+end
 
 def add_pet_to_stock(pet_shop, new_pet)
   pet_shop[:pets] << new_pet
@@ -67,5 +67,19 @@ def customer_can_afford_pet(customer, new_pet)
     return true
   else
     return false
+  end
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  return if find_pet_by_name(pet_shop, pet)
+
+  customer_can_afford_pet(customer, new_pet)
+  amount = pet[:price]
+  add_pet_to_customer(customer, pet)
+  remove_pet_by_name(pet_shop, name)
+  increase_pets_sold(pets_sold, 1)
+  add_or_remove_cash(pet_shop, amount)
+
+
   end
 end
